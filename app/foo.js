2379ac728fa46da2b3a50acfccb4cc6b56ebf67d
杨柳 <1545766219@qq.com>
2017-04-13 09:57:44 +0800

    添加foo.js文件

---
+++ app/foo.js
@@ -0,0 +1,6 @@
+/**
+ * Created by yangliu on 2017/4/13.
+ */
+export default function(){
+      return '方方的教程'
+      }
---
+++ app/index.js
@@ -0,0 +1,16 @@
+/**
+ * Created by yangliu on 2017/4/13.
+ */
+import _ from 'lodash';
+import j from 'jquery';
+import foo from './foo'
+function component () {
+    var element = j('<div></div>');
+    /* lodash is required for the next line to work */
+    element.html(_.join(['Hello','杨柳'], ' '))
+    return element.get(0);
+}
+
+document.body.appendChild(component());
+console.log(foo)
+console.log(foo()) //打开浏览器控制台看看结果
